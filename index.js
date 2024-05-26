8a76ea92013d4a4f6f0685ba632db9a65586ce74
Braxgjider <168082518+Braxgjider@users.noreply.github.com>
2024-05-26 22:51:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
+let model, webcam, prediccions, maxPrediccions, geoID;
+
+function inicia_sessio() {
+    const username = document.querySelector('#nom_usuari').value;
+    const password = document.querySelector('#contrasenya').value;
+    // Lógica para el inicio de sesión
+    console.log(`Inicio de sesión con usuario: ${username}`);
+}
+
+function nou_usuari() {
+    // Lógica para crear un nuevo usuario
+    console.log('Creación de nuevo usuario');
+}
+
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
-    const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
-    for (let i = 1; i < num_botons; i++) {
+    const num_botons = menu.children.length;
+    for (let i = 1; i <= num_botons; i++) {
         let boto = document.getElementById("boto_" + i);
         let seccio = document.getElementById("seccio_" + i);
-        if (i == num_boto) {
-            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+        if (i === num_boto) {
+            boto.style.color = "#950E17";
             boto.style.backgroundColor = "#FCDEE0";
-            seccio.style.display = "flex";    // es fa visible la secció activa
-        }
-        else {
-            boto.style.color = "white";    // colors dels botons de seccions inactives
+            seccio.style.display = "flex";
+        } else {
+            boto.style.color = "white";
             boto.style.backgroundColor = "#950E17";
-            seccio.style.display = "none";    // s'oculten les seccions inactives
+            seccio.style.display = "none";
         }
+    }
+
+    if (num_boto === 4) {
+        mapa.invalidateSize();
+        if (typeof geoID === "undefined") {
+            navigator.geolocation.watchPosition(geoExit);
+        }
+    }
+}
+
+function tanca_sessio() {
+    // Lógica para cerrar sesión
+    console.log('Sesión cerrada');
+}
+
+function geoExit(posicio) {
+    let latitud = posicio.coords.latitude;
+    let longitud = posicio.coords.longitude;
+    if (typeof geoID === "undefined") {
+        geoID = L.marker([latitud, longitud], {zIndexOffset:100, title:"Usuari"}).addTo(mapa);
+    } else {
+        geoID.setLatLng([latitud, longitud]);
     }
 }
